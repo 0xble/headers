3a504c6a77ad20c65e20825739ff65b04993ee4e
Brian Le <brian@brianle.xyz>
2022-11-27 12:28:00 -0800

    fix padding for headers with odd length

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,23 @@ use clipboard::{ClipboardContext, ClipboardProvider};
 
 fn main() {
     let input = env::args().collect::<Vec<String>>()[1..].join(" ");
-    let padding = (0..(60 - input.len()) / 2).map(|_| " ").collect::<String>();
+
+    let l_padding = (0..(60 - input.len()) / 2).map(|_| " ").collect::<String>();
+
+    let mut r_padding = l_padding.clone();
+    if input.len() % 2 != 0 {
+        r_padding.push(' ');
+    }
 
     let output = format!(
-        "{}\n{}{}{}{}\n{}",
-        "    ////////////////////////////////////////////////////////////////",
-        "    //",
-        padding.clone(),
+        "{}\n{}{}{}{}{}\n{}",
+        "////////////////////////////////////////////////////////////////",
+        "//",
+        l_padding,
         input.to_uppercase(),
-        padding + "//",
-        "    ////////////////////////////////////////////////////////////////"
+        r_padding,
+        "//",
+        "////////////////////////////////////////////////////////////////"
     );
 
     println!("{}", output); // Print the header to console.
